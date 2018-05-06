a7c5c75da854f619c0a2c5809313d6e5ef4a83cc
Naveed Baqai <naveed@hibirdi.com>
2018-05-06 12:45:20 +0100

    set contact form with jQuery Validation

---
+++ src/js/contactFormValidation.js
@@ -0,0 +1,32 @@
+$(function () {
+    $('#contactForm').validate({
+      rules: {
+          firstName: {
+              required: true
+          },
+          lastName: {
+              required: true
+          },
+          message: {
+              required: true
+          }
+      },
+      messages: {
+         firstName: {required: "Please enter your first name!"},
+         lastName: {required: "Please enter your last name!"},
+         message: {required: "Please enter your message!"}
+      },
+      submitHandler: function() {
+        $("#contactFormOnSuccess").removeClass("hide").html("<strong>Thank You</strong><br/> We will get back to you soon");
+        $('#contact-form').trigger("reset");
+      }
+  });
+});
+
+$('#contactForm').on('keyup change paste', 'input, textarea', function(){
+    $("#contactFormOnSuccess").addClass("hide").html("");
+});
+
+$('#contactFormSubmit').click(function() {
+  $("#contactFormOnSuccess").addClass("hide").html("");
+});
