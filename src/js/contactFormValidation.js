20a90c55cd13b7e0adc51c4ad1866793f668d959
Naveed Baqai <naveed@hibirdi.com>
2018-05-08 11:05:27 +0100

    clean up the code

--- src/js/contactFormValidation.js
+++ src/js/contactFormValidation.js
@@ -1,32 +1,41 @@
-$(function () {
+$(function() {
     $('#contactForm').validate({
-      rules: {
-          firstName: {
-              required: true
-          },
-          lastName: {
-              required: true
-          },
-          message: {
-              required: true
-          }
-      },
-      messages: {
-         firstName: {required: "Please enter your first name!"},
-         lastName: {required: "Please enter your last name!"},
-         message: {required: "Please enter your message!"}
-      },
-      submitHandler: function() {
-        $("#contactFormOnSuccess").removeClass("hide").html("<strong>Thank You</strong><br/> We will get back to you soon");
-        $('#contact-form').trigger("reset");
-      }
-  });
+        rules: {
+            firstName: {
+                required: true
+            },
+            lastName: {
+                required: true
+            },
+            message: {
+                required: true
+            }
+        },
+        messages: {
+            firstName: {
+                required: "Please enter your first name!"
+            },
+            lastName: {
+                required: "Please enter your last name!"
+            },
+            message: {
+                required: "Please enter your message!"
+            }
+        },
+        submitHandler: function() {
+            $('#contactFormOnSuccess').removeClass('hide').html("<strong>Thank You</strong><br/> We will get back to you soon");
+            $('#contactForm').trigger('reset');
+            $('html, body').animate({
+                scrollTop: $('#contactForm').offset().top
+            }, 1000);
+        }
+    });
 });
 
-$('#contactForm').on('keyup change paste', 'input, textarea', function(){
-    $("#contactFormOnSuccess").addClass("hide").html("");
+$('#contactForm').on('keyup change paste', 'input, textarea', function() {
+    $('#contactFormOnSuccess').addClass('hide').html("");
 });
 
 $('#contactFormSubmit').click(function() {
-  $("#contactFormOnSuccess").addClass("hide").html("");
+    $('#contactFormOnSuccess').addClass('hide').html("");
 });
